467d0c689e58b8de6e6ac6fec62fff6b5ee91e41
YutaTomizuka <bookin12@gmail.com>
2016-03-23 22:29:36 +0900

    一覧画面モック作成

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-// var main = function(){
-//   this.date = '3/21';
-// };
-
-
 angular.module('myApp', ['mgcrea.ngStrap'])
-.controller('MainController',function(){
-  this.date = '3/21';
-});
+  .controller('MainController', function() {
+    this.currentdate = "2016年3月23日";
+    this.reserves = [{
+      date: "2016年3月23日",
+      start: "15:00",
+      end: "17:00",
+      id: "0001",
+      name: "富塚陽子ちゃん"
+    }, {
+      date: "2016年3月23日",
+      start: "15:00",
+      end: "17:00",
+      id: "0002",
+      name: "富塚陽太くん"
+    }, {
+      date: "2016年3月24日",
+      start: "15:00",
+      end: "17:00",
+      id: "0003",
+      name: "富塚祐太くん"
+    }, {
+      date: "2016年3月24日",
+      start: "15:00",
+      end: "17:00",
+      id: "0002",
+      name: "富塚たくまくん"
+    }];
+  });
