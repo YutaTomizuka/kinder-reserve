f7b45e98c75dcecae415d8b57e66605916da9b10
YutaTomizuka <bookin12@gmail.com>
2016-03-24 22:11:05 +0900

    日付移動を追加

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
 angular.module('myApp', ['mgcrea.ngStrap'])
   .controller('MainController', function() {
-    this.currentdate = "2016年3月23日";
+    var now = new Date();
+    this.currentdate = comDateFormat(now, "yyyy年MM月dd日");
     this.reserves = [{
-      date: "2016年3月23日",
+      date: "2016年03月23日",
       start: "15:00",
       end: "17:00",
       id: "0001",
       name: "富塚陽子ちゃん"
     }, {
-      date: "2016年3月23日",
+      date: "2016年03月23日",
       start: "15:00",
       end: "17:00",
       id: "0002",
       name: "富塚陽太くん"
     }, {
-      date: "2016年3月24日",
+      date: "2016年03月24日",
       start: "15:00",
       end: "17:00",
       id: "0003",
       name: "富塚祐太くん"
     }, {
-      date: "2016年3月24日",
+      date: "2016年03月24日",
       start: "15:00",
       end: "17:00",
       id: "0002",
       name: "富塚たくまくん"
     }];
+    this.nextDay = function(){
+      now.setDate(now.getDate() + 1);
+      this.currentdate = comDateFormat(now, "yyyy年MM月dd日");
+    }
+    this.preDay = function(){
+      now.setDate(now.getDate() - 1);
+      this.currentdate = comDateFormat(now, "yyyy年MM月dd日");
+    }
   });
