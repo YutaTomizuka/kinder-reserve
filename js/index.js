8e4be5a22fd9e3e49d2917ff2d8bb70c25d68a68
YutaTomizuka <bookin12@gmail.com>
2016-05-05 20:59:12 +0900

    Herokuに立てたapiと連携

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
-function mainCtrl($uibModal){
+function mainCtrl($http, $uibModal){
 
   var that = this;
   var now = new Date();
   this.currentdate = comDateFormat(now, "yyyy年MM月dd日");
-  this.reserves = [{
-    date: "2016年03月23日",
-    start: "15:00",
-    end: "17:00",
-    id: "0001",
-    name: "富塚陽子ちゃん"
-  }, {
-    date: "2016年03月23日",
-    start: "15:00",
-    end: "17:00",
-    id: "0002",
-    name: "富塚陽太くん"
-  }, {
-    date: "2016年03月24日",
-    start: "15:00",
-    end: "17:00",
-    id: "0003",
-    name: "富塚祐太くん"
-  }, {
-    date: "2016年03月24日",
-    start: "15:00",
-    end: "17:00",
-    id: "0002",
-    name: "富塚たくまくん"
-  }];
+  // this.reserves = [{
+  //   date: "2016年05月03日",
+  //   start: "15:00",
+  //   end: "17:00",
+  //   id: "0001",
+  //   name: "富塚陽子ちゃん"
+  // }, {
+  //   date: "2016年05月04日",
+  //   start: "15:00",
+  //   end: "17:00",
+  //   id: "0002",
+  //   name: "富塚陽太くん"
+  // }, {
+  //   date: "2016年05月04日",
+  //   start: "15:00",
+  //   end: "17:00",
+  //   id: "0003",
+  //   name: "富塚祐太くん"
+  // }, {
+  //   date: "2016年05月05日",
+  //   start: "15:00",
+  //   end: "17:00",
+  //   id: "0002",
+  //   name: "富塚たくまくん"
+  // }];
+that = this;
+  $http({
+        method: 'GET',
+        // url: 'http://localhost:3000/api/test'
+        url: 'https://nameless-meadow-41004.herokuapp.com/api/test'
+      })
+      // 成功時の処理（ページにあいさつメッセージを反映）
+      .success(function(data, status, headers, config){
+        that.reserves = data;
+      })
+      // 失敗時の処理（ページにエラーメッセージを反映）
+      .error(function(data, status, headers, config){
+      });
+
   this.nextDay = function(){
     now.setDate(now.getDate() + 1);
     this.currentdate = comDateFormat(now, "yyyy年MM月dd日");
@@ -83,5 +97,5 @@ function modalCtrl($uibModalInstance,data)
 }
 
 angular.module('myApp', ['ui.bootstrap'])
-.controller('MainController',['$uibModal', mainCtrl])
+.controller('MainController',['$http', '$uibModal', mainCtrl])
 .controller('ModalController', ['data','$uibModalInstance', modalCtrl]);
